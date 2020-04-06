863ad8586b0dd6ef50345e0ec7b7d7d71ccff53e
Gregory Sanders <gsanders87@gmail.com>
2020-04-06 15:23:53 -0400

    Add feeloop.py

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CoreFeeHelper
 Muh Twitter feebot @CoreFeeHelper
+
+How to install:
+```
+git clone git@github.com:instagibbs/CoreFeeHelper.git
+cd CoreFeeHelper
+pip install tweepy # Or install your own favorite way
+```
+
+Edit the tweepy fields that are `FIXME` to get Twitter auth working.
+
+```
+./feeloop.py <RPC username> <RPC password>
+```
+
+Profit.
---
+++ feeloop.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import time
+from authproxy import AuthServiceProxy
+import tweepy
+import random
+import urllib.request
+import json
+import re
+import sys
+
+# Requires running Core RPC server, tweepy auth fill out below
+if len(sys.argv) < 3:
+    raise Exception('feeloop.py <RPC username> <RPC password>')
+
+while True:
+    bitcoin_req = "http://"+sys.argv[1]+":"+sys.argv[2]+"@127.0.0.1:8332"
+    bitcoin = AuthServiceProxy(bitcoin_req)
+
+    def get_rounded_feerate(result):
+        rate = str(int(result*1000000)/10.0)+" sat/byte "
+        if len(re.split("\.", rate)[0]) == 1:
+            rate = " "+rate
+        return rate
+
+    try:
+        nextblock = ["Next: ", bitcoin.estimatesmartfee(1, "ECONOMICAL")["feerate"]]
+        hour = ["1h:   ", bitcoin.estimatesmartfee(6, "ECONOMICAL")["feerate"]]
+        six_hours = ["6h:   ", bitcoin.estimatesmartfee(6*6, "ECONOMICAL")["feerate"]]
+        twelve_hours = ["12h:  ", bitcoin.estimatesmartfee(6*12, "ECONOMICAL")["feerate"]]
+        day = ["1d:   ", bitcoin.estimatesmartfee(144, "ECONOMICAL")["feerate"]]
+        half_week = ["3d:   ", bitcoin.estimatesmartfee(int(144*3.5), "ECONOMICAL")["feerate"]]
+        week = ["1wk:  ", bitcoin.estimatesmartfee(144*7, "ECONOMICAL")["feerate"]]
+
+        bitstampprice = urllib.request.urlopen("https://www.bitstamp.net/api/v2/ticker/btcusd/").read()
+        latest_price = float(json.loads(bitstampprice)["last"])
+        price_for_250 = latest_price/4 # Price for 250 byte tx
+
+        tweet = ""
+        for estimate in [nextblock, hour, six_hours, twelve_hours, day, half_week, week]:
+            tweet += estimate[0]+get_rounded_feerate(estimate[1]) + " ${}".format(round(price_for_250*float(estimate[1]),2))+"\n"
+
+        count_str = f"{bitcoin.getblockcount():,d}"
+        tweet += "Block height: "+ count_str+"\n"
+        tweet += "Mempool depth: "+str(int(bitcoin.getmempoolinfo()["bytes"]/1000/1000))
+        if bitcoin.getblockcount() % 8 == 0:
+            tweet += "\nTip me! https://tippin.me/@CoreFeeHelper"
+
+    except Exception as e:
+        print("Couldn't estimate. Sleeping: {}".format(str(e)))
+        time.sleep(3600)
+        continue
+
+    print("Getting tweepy auth")
+    # See http://docs.tweepy.org/en/latest/
+    auth = tweepy.OAuthHandler("FIXME", "FIXME")
+    auth.set_access_token("FIXME", "FIXME")
+    api = tweepy.API(auth)
+
+    try:
+        api.update_status(tweet)
+        print(tweet)
+    except tweepy.TweepError as err:
+        print("Error: "+str(err))
+        print(tweet)
+        print("------------------")
+        pass
+
+
+    time.sleep(3600)
